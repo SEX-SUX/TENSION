b7ff56779e202023f97932fa45e15ddc39ed62a8
SEX-SUX <sachinxpapa@gmail.com>
2024-10-19 20:56:08 +0530

    All Set !

--- AnonXMusic/ai/chatgpt.py
+++ AnonXMusic/ai/chatgpt.py
@@ -5,7 +5,7 @@ from AnonXMusic import app
 import requests
 from pyrogram.types import  Message
 from pyrogram.types import InputMediaPhoto
-from SACHINxAPI import api
+from teambabyAPI import api
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
