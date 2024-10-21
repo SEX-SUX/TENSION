634fb731eabf70367fc51a582e9095e1fc335481
SEX-SUX <sachinxpapa@gmail.com>
2024-10-21 13:59:58 +0530

    All Set !

--- AnonXMusic/plugins/tools/queue.py
+++ AnonXMusic/plugins/tools/queue.py
@@ -8,7 +8,7 @@ from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 import config
 from AnonXMusic import app
 from AnonXMusic.misc import db
-from AnonXMusic.utils import AnonyBin, get_channeplayCB, seconds_to_min
+from AnonXMusic.utils import Bin, get_channeplayCB, seconds_to_min
 from AnonXMusic.utils.database import get_cmode, is_active_chat, is_music_playing
 from AnonXMusic.utils.decorators.language import language, languageCB
 from AnonXMusic.utils.inline import queue_back_markup, queue_markup
@@ -176,7 +176,7 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
             return await CallbackQuery.edit_message_text(msg, reply_markup=buttons)
         if "✨" in msg:
             msg = msg.replace("✨", "")
-        link = await AnonyBin(msg)
+        link = await Bin(msg)
         med = InputMediaPhoto(media=link, caption=_["queue_3"].format(link))
         await CallbackQuery.edit_message_media(media=med, reply_markup=buttons)
     else:
